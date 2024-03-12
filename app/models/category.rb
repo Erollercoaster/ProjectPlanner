28de2d9cf735bf14623bfcb84d9c250ab9fe154a
Erollercoaster <erollfern@gmail.com>
2024-03-12 18:09:59 +0800

    final changes

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,4 @@ class Category < ApplicationRecord
     belongs_to :user
     has_many :tasks, dependent: :destroy
 
-    validates :name, presence: true
-    validates :description, presence: true
-
 end
